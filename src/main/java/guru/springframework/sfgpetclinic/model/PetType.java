76f6c83681c70ff44ff5b6d6e0af73b78d3c7237
Gowtham-chidambaranathan <gowtham.chidambaranathan@freshworks.com>
2021-05-07 20:34:07 +0530

    pojos added

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,17 @@
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ * Created by Gowtham C on 07/05/21.
+ */
+public class PetType {
+
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ * Created by Gowtham C on 07/05/21.
+ */
+public class Vet extends Person {
+}
