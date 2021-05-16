4e100dc9c81f3db3278efe2206438b2443e4926a
Gowtham-chidambaranathan <gowtham.chidambaranathan@freshworks.com>
2021-05-16 23:37:20 +0530

    bootstrap data:

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
@@ -1,9 +1,11 @@
 package guru.springframework.sfgpetclinic.services;
 
+import guru.springframework.sfgpetclinic.model.Vet;
+
 /**
  * Created by Gowtham C on 08/05/21.
  */
-public interface VetService extends CrudService<VetService, Long> {
+public interface VetService extends CrudService<Vet, Long> {
 
 
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -2,13 +2,14 @@ package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.CrudService;
+import guru.springframework.sfgpetclinic.services.VetService;
 
 import java.util.Set;
 
 /**
  * Created by Gowtham C on 08/05/21.
  */
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
 
   @Override
   public Set<Vet> findAll() {
