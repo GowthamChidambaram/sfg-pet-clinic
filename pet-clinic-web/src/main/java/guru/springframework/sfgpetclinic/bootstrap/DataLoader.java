f7610149c40ec9bd78b60e3b05dfca894410e819
Gowtham-chidambaranathan <gowtham.chidambaranathan@freshworks.com>
2021-05-17 00:32:22 +0530

    List owners

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -36,11 +36,11 @@ public class DataLoader implements CommandLineRunner {
     ownerService.save(owner1);
 
     Owner owner2 = new Owner();
-    owner2.setId(1L);
+    owner2.setId(2L);
     owner2.setFirstName("Fiona");
     owner2.setLastName("Glennane");
 
-    ownerService.save(owner1);
+    ownerService.save(owner2);
 
     System.out.println("Loaded owners...!!!!");
 
@@ -52,7 +52,7 @@ public class DataLoader implements CommandLineRunner {
     vetService.save(vet1);
 
     Vet vet2 = new Vet();
-    vet2.setId(1L);
+    vet2.setId(2L);
     vet2.setFirstName("Sam");
     vet2.setLastName("Axe");
 
